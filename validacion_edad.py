48039d1bd57c33a4d89a4e80fc8d40fe3d800081
erxzz <205370246+erxzz@users.noreply.github.com>
2025-10-29 17:00:56 -0500

    archivo validacion modificado

--- validacion_edad.py
+++ validacion_edad.py
@@ -1,5 +1,9 @@
-edad=input("Ingrese su edad: ")
-if edad <18: 
-    print("No tiene la edad suficiente para entrar a la discoteca.")
-else:
-    print("Acceso otorgado.")
+def discoteca(edad):
+    if edad <18:
+        return "Acceso denegado , el usuario es menor de edad"
+    else:
+        return "Acceso otorgado"
+fila = {"Andres":18,"Felipe":17,"Sara":16,"Samuel":20,"Daniel":19}
+for persona in fila:
+    acceso= discoteca(fila[persona])
+    print(f"{acceso}, ({persona})")
